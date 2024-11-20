cf12e2c7b82d18e5661990fad043da5b704572b5
frontenddev344 <frontenddev344@gmail.com>
2024-11-20 11:59:19 +0530

    updated

--- assets/php/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    unset($_SESSION["user"]);
-    setcookie("email",' ',1,"/");
-    setcookie("password",' ',1,"/");
-    header("location:/Soccer-Spotlight/login.php")
-
-?>
